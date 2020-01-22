99d340c5aba796819ad31fba83c416540d929d4d
Brian Nicholls <nichol88@gmail.com>
2020-01-22 02:30:21 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,7 +36,7 @@ def select_series_title_with_most_human_characters
   FROM series
   JOIN books ON books.id = series.book_id
   JOIN character_books ON series.book_id = books.id
-  HAVING characters.species = 'human'
+  WHERE characters.species = 'human'
   GROUP BY characters.species
   LIMIT 1
   ;"
